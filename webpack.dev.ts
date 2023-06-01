cbecacb3e902c009a18d26fc869f2902bc5baf7d
andre2xu <>
2023-06-01 11:38:33 +0100

    Added watch configs.

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -2,5 +2,9 @@ const { merge } = require('webpack-merge');
 const common_configs = require('./webpack.config.js');
 
 module.exports = merge(common_configs, {
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/,
+  },
   mode: 'development'
 });
