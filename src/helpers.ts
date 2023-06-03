3435bc47af6003ec41cdb5f4021b9fa4cf2d2171
= <=>
2023-06-03 16:20:31 +0100

    Added return types to helpers.

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,13 +1,14 @@
 import $ from 'jquery';
+import * as shared_types from './shared_types';
 
 
 
-function updateComponentCanvasDimensions(jscp_component: HTMLElement, component_canvas: HTMLCanvasElement) {
+function updateComponentCanvasDimensions(jscp_component: HTMLElement, component_canvas: HTMLCanvasElement): void {
   component_canvas.width = jscp_component.offsetWidth;
   component_canvas.height = jscp_component.offsetHeight;
 };
 
-function getMousePositionRelativeToElement(element: HTMLElement, mouse_x: number, mouse_y: number) {
+function getMousePositionRelativeToElement(element: HTMLElement, mouse_x: number, mouse_y: number): shared_types.Coordinates {
   return {
     x: mouse_x - element.offsetLeft,
     y: mouse_y - element.offsetTop
