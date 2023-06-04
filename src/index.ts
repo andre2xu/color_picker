4cefdeec0ad7d5fcf11513e14fd2f390359c3972
= <=>
2023-06-04 12:56:14 +0100

    Moved 'mousedown' callback to a file dedicated for event handlers.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import helpers from './helpers';
+import event_handlers from './event_handlers';
 import * as shared_types from './shared_types';
 
 
@@ -109,38 +110,7 @@ class JSColorPicker {
     }
 
     // binds event listeners
-    this.color_picker.on('mousedown', function (this: JSColorPicker, event: JQuery.TriggeredEvent) {
-      const CLICKED_ELEMENT: HTMLElement = event.target;
-
-      if (event.button === 0 && event.clientX !== undefined && event.clientY !== undefined) {
-        const MOUSE_POSITION: shared_types.Coordinates = helpers.getMousePositionRelativeToElement(
-          event.target,
-          event.clientX,
-          event.clientY
-        );
-
-        if (this.hues !== undefined && this.hues[0] === CLICKED_ELEMENT && this.hue_slider !== undefined) {
-          helpers.moveVerticalSlider(
-            this.hue_slider[0],
-            MOUSE_POSITION.y
-          );
-        }
-        else if (this.alpha_channel !== undefined && this.alpha_channel[0] === CLICKED_ELEMENT && this.ac_slider !== undefined) {
-          const AC_CONTAINER: HTMLElement = event.target.parentElement as HTMLElement;
-
-          helpers.moveVerticalSlider(
-            this.ac_slider[0],
-            event.clientY - AC_CONTAINER.offsetTop
-          );
-        }
-        else if (this.hue_slider !== undefined && this.hue_slider[0] === CLICKED_ELEMENT) {
-          this.component_held = this.hue_slider;
-        }
-        else if (this.ac_slider !== undefined && this.ac_slider[0] === CLICKED_ELEMENT) {
-          this.component_held = this.ac_slider;
-        }
-      }
-    }.bind(this));
+    this.color_picker.on('mousedown', event_handlers.mouseDownHandler.bind(this));
 
     this.color_picker.on('mouseup', function (this: JSColorPicker) {
       this.component_held = undefined;
