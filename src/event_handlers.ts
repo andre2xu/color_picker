61f454463e5eb7c427e186aa7cb35ac38f5764ed
andre2xu <>
2023-06-04 15:43:49 +0100

    The shade & tint component cursor is now draggable.

--- src/event_handlers.ts
+++ src/event_handlers.ts
@@ -77,6 +77,22 @@ function mouseMoveHandler(this: JSColorPicker, event: JQuery.TriggeredEvent) {
         );
       }
     }
+    else if (this.snt_cursor !== undefined && this.component_held[0] === this.snt_cursor[0] && this.shades_and_tints !== undefined) {
+      const SNT_COMPONENT: HTMLElement = this.shades_and_tints[0];
+      const MOUSE_POSITION = helpers.getMousePositionRelativeToElement(
+        SNT_COMPONENT,
+        event.clientX,
+        event.clientY
+      );
+
+      if (MOUSE_POSITION.x <= SNT_COMPONENT.offsetWidth && MOUSE_POSITION.y <= SNT_COMPONENT.offsetHeight) {
+        helpers.moveSNTCursor(
+          this.snt_cursor[0],
+          MOUSE_POSITION.x,
+          MOUSE_POSITION.y
+        );
+      }
+    }
   }
 };
 
